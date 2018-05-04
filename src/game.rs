30a087a5371a6f60e70d86fff792d855619d5f24
Adolfo <arthur.adolfo@sap.com>
2018-05-04 15:30:23 -0300

    [ADD] Create check collision method in Game struct

--- src/game.rs
+++ src/game.rs
@@ -11,6 +11,20 @@ impl Game {
             score: 0,
         }
     }
+    
+    pub fn check_collision(&self, box_one: ((f64,f64),(f64,f64)), box_two: ((f64,f64),(f64,f64))) -> bool {
+        if (box_one.0).0 >= (box_two.0).0 && (box_one.0).0 <= (box_two.1).0 {
+            if (box_one.0).1 >= (box_two.0).1 && (box_one.0).1 <= (box_two.1).1 {
+                return true;
+            }
+        }
+        if (box_one.1).0 >= (box_two.0).0 && (box_one.1).0 <= (box_two.1).0 {
+            if (box_one.1).1 >= (box_two.0).1 && (box_one.1).1 <= (box_two.1).1 {
+                return true;
+            }
+        }
+        return false;
+    }
 
     pub fn update(&mut self, dt: f64) {
     }
