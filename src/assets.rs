b016ca5fbe6c10317927ea35b2c8b34755c230a1
Leonardo Nahra <leonardonahra@gmail.com>
2018-05-15 08:47:40 -0300

    Unwrap assets loaded

--- src/assets.rs
+++ src/assets.rs
@@ -37,166 +37,113 @@ impl Assets {
         let settings = TextureSettings::new();
 
         let font =
-            match GlyphCache::new(
+            GlyphCache::new(
                 fonts.join("space_invaders.ttf"),
                 (),
-                settings.clone()) {
-
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+                settings.clone()
+            ).unwrap();
 
         let canon =
-            match Texture::from_path(images.join("canon.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("canon.png"), &settings)
+                .unwrap();
 
         let alpha_up =
-            match Texture::from_path(images.join("alpha_up.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("alpha_up.png"), &settings)
+                .unwrap();
 
         let alpha_down =
-            match Texture::from_path(images.join("alpha_down.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("alpha_down.png"), &settings)
+                .unwrap();
 
         let beta_up =
-            match Texture::from_path(images.join("beta_up.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("beta_up.png"), &settings)
+                .unwrap();
 
         let beta_down =
-            match Texture::from_path(images.join("beta_down.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("beta_down.png"), &settings)
+                .unwrap();
 
         let gamma_up =
-            match Texture::from_path(images.join("gamma_up.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("gamma_up.png"), &settings)
+                .unwrap();
 
         let gamma_down =
-            match Texture::from_path(images.join("gamma_down.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("gamma_down.png"), &settings)
+                .unwrap();
 
         let explosion =
-            match Texture::from_path(images.join("explosion.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("explosion.png"), &settings)
+                .unwrap();
 
         let spaceship =
-            match Texture::from_path(images.join("spaceship.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("spaceship.png"), &settings)
+                .unwrap();
 
         let bullet =
-            match Texture::from_path(images.join("bullet.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(images.join("bullet.png"), &settings)
+                .unwrap();
 
         let bunker = images.join("bunker");
 
         let block_full =
-            match Texture::from_path(bunker.join("full.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("full.png"), &settings)
+                .unwrap();
 
         let block_full_bl =
-            match Texture::from_path(bunker.join("full_bl.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("full_bl.png"), &settings)
+                .unwrap();
 
         let block_full_br =
-            match Texture::from_path(bunker.join("full_br.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("full_br.png"), &settings)
+                .unwrap();
 
         let block_full_tl =
-            match Texture::from_path(bunker.join("full_tl.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("full_tl.png"), &settings)
+                .unwrap();
 
         let block_full_tr =
-            match Texture::from_path(bunker.join("full_tr.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("full_tr.png"), &settings)
+                .unwrap();
 
         let block_half =
-            match Texture::from_path(bunker.join("half.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("half.png"), &settings)
+                .unwrap();
 
         let block_half_bl =
-            match Texture::from_path(bunker.join("half_bl.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("half_bl.png"), &settings)
+                .unwrap();
 
         let block_half_br =
-            match Texture::from_path(bunker.join("half_br.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("half_br.png"), &settings)
+                .unwrap();
 
         let block_half_tl =
-            match Texture::from_path(bunker.join("half_tl.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("half_tl.png"), &settings)
+                .unwrap();
 
         let block_half_tr =
-            match Texture::from_path(bunker.join("half_tr.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("half_tr.png"), &settings)
+                .unwrap();
 
         let block_weak =
-            match Texture::from_path(bunker.join("weak.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("weak.png"), &settings)
+                .unwrap();
 
         let block_weak_bl =
-            match Texture::from_path(bunker.join("weak_bl.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("weak_bl.png"), &settings)
+                .unwrap();
 
         let block_weak_br =
-            match Texture::from_path(bunker.join("weak_br.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("weak_br.png"), &settings)
+                .unwrap();
 
         let block_weak_tl =
-            match Texture::from_path(bunker.join("weak_tl.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("weak_tl.png"), &settings)
+                .unwrap();
 
         let block_weak_tr =
-            match Texture::from_path(bunker.join("weak_tr.png"), &settings) {
-                Ok(t) => t,
-                Err(e) => panic!(e.to_string())
-            };
+            Texture::from_path(bunker.join("weak_tr.png"), &settings)
+                .unwrap();
 
         Assets {
             font,
